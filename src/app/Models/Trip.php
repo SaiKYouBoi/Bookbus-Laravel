ae12a76eef8b4668bd74c5fb10fedb1f4b83d26b
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-02 14:53:46 +0100

    Added relationships to Trip Model

--- src/app/Models/Trip.php
+++ src/app/Models/Trip.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Trip extends Model
 {
     use HasFactory;
+
+    public function route()
+    {
+        return $this->belongsTo(Route::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(BusCompany::class, 'company_id');
+    }
+
+    public function driver()
+    {
+        return $this->belongsTo(Driver::class);
+    }
+
+    public function segments()
+    {
+        return $this->hasMany(TripSegment::class);
+    }
+
+    public function tripSeats()
+    {
+        return $this->hasMany(TripSeat::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
