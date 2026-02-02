1fdd6fb565e75d30b52b169f64edff1f9e46b7b5
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-02 14:53:57 +0100

    Added relationships to TripSeat Model

--- src/app/Models/TripSeat.php
+++ src/app/Models/TripSeat.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class TripSeat extends Model
 {
     use HasFactory;
+
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+
+    public function seat()
+    {
+        return $this->belongsTo(Seat::class);
+    }
+
+    public function bookingSeats()
+    {
+        return $this->hasMany(BookingSeat::class);
+    }
 }
