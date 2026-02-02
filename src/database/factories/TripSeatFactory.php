c54f87ed602c7b968976e28b75b63890b4b52cfb
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-02 10:08:20 +0100

    Added TripSeatFactory and TripSegmentFactory

---
+++ src/database/factories/TripSeatFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Seat;
+use App\Models\Trip;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TripSeat>
+ */
+class TripSeatFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'trip_id' => Trip::factory(),
+            'seat_id' => Seat::factory(),
+        ];
+    }
+}
