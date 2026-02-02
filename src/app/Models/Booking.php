eeb583cb5ae2e9948cc4c0839f40bc6f5b559fee
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-02 14:51:33 +0100

    Added relationships to Booking Model

--- src/app/Models/Booking.php
+++ src/app/Models/Booking.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+
+    public function bookingSeats()
+    {
+        return $this->hasMany(BookingSeat::class);
+    }
 }
