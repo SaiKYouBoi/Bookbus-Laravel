e6e2f0c4f2d1b556f0600b4d7ecaa81324402c95
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-02 14:53:38 +0100

    Added relationships to Station Model

--- src/app/Models/Station.php
+++ src/app/Models/Station.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Station extends Model
 {
     use HasFactory;
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function routeStops()
+    {
+        return $this->hasMany(RouteStop::class);
+    }
 }
