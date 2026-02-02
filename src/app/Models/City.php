01b0454675cd8767fc38cb4f88045522bcce0780
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-02 14:52:32 +0100

    Added relationships to City Model

--- src/app/Models/City.php
+++ src/app/Models/City.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    public function stations()
+    {
+        return $this->hasMany(Station::class);
+    }
+
+    public function departureBookings()
+    {
+        return $this->hasMany(Booking::class, 'departure_city_id');
+    }
+
+    public function arrivalBookings()
+    {
+        return $this->hasMany(Booking::class, 'arrival_city_id');
+    }
 }
