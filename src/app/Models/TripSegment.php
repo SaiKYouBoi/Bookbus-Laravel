2b20ef08ced417abf39b15dab6b60f691e75cdb0
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-02 14:54:10 +0100

    Added relationships to TripSegment Model

--- src/app/Models/TripSegment.php
+++ src/app/Models/TripSegment.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class TripSegment extends Model
 {
     use HasFactory;
+
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+
+    public function fromStop()
+    {
+        return $this->belongsTo(RouteStop::class, 'from_stop_id');
+    }
+
+    public function toStop()
+    {
+        return $this->belongsTo(RouteStop::class, 'to_stop_id');
+    }
 }
