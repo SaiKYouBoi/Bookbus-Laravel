d44185fad91b265844138b38d0e05f6224f9ab0c
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-02 14:53:11 +0100

    Added relationships to RouteStop Model

--- src/app/Models/RouteStop.php
+++ src/app/Models/RouteStop.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class RouteStop extends Model
 {
     use HasFactory;
+
+    public function route()
+    {
+        return $this->belongsTo(Route::class);
+    }
+
+    public function station()
+    {
+        return $this->belongsTo(Station::class);
+    }
+
+    public function fromSegments()
+    {
+        return $this->hasMany(TripSegment::class, 'from_stop_id');
+    }
+
+    public function toSegments()
+    {
+        return $this->hasMany(TripSegment::class, 'to_stop_id');
+    }
 }
