6bcc4463f3373c3758c423476b5d6de15ddd9cef
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-05 12:09:53 +0100

    refactor: Bus Model

---
+++ src/app/Models/Bus.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bus extends Model
+{
+    use HasFactory;
+
+    public function trips()
+    {
+        return $this->hasMany(Trip::class, 'company_id');
+    }
+
+    public function services()
+    {
+        return $this->hasMany(Service::class, 'bus_id');
+    }
+}
