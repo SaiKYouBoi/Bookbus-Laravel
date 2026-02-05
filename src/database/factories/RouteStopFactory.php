c7848c906bf891d1bcf5bb5cfc1bf74454139161
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-05 12:07:36 +0100

    refactor: RouteStopFactory

--- src/database/factories/RouteStopFactory.php
+++ src/database/factories/RouteStopFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Route;
+use App\Models\RouteStop;
+use App\Models\Station;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +17,25 @@ class RouteStopFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    protected $model = RouteStop::class;
+
+    public function definition()
     {
+
+        $route = Route::inRandomOrder()->first();
+
+        $station = Station::inRandomOrder()->first();
+
+        $lastStop = RouteStop::where('route_id', $route->id)
+            ->orderBy('stop_order', 'desc')
+            ->first();
+
+        $stopOrder = $lastStop ? $lastStop->stop_order + 1 : 1;
+
         return [
-            //
+            'route_id' => $route->id,
+            'station_id' => $station->id,
+            'stop_order' => $stopOrder,
         ];
     }
 }
