5f7133e906214633e00ef7bf0cfe4fc119855c74
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-05 12:05:19 +0100

    refactor: TripFactory

--- src/database/factories/TripFactory.php
+++ src/database/factories/TripFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Bus;
+use App\Models\Driver;
+use App\Models\Route;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +19,17 @@ class TripFactory extends Factory
      */
     public function definition(): array
     {
+        $departure = fake()->dateTimeBetween('+1 days', '+1 month');
+        $arrival = (clone $departure)->modify('+3 hours');
+
         return [
-            //
+            'route_id' => Route::inRandomOrder()->value('id'),
+            'bus_id' => Bus::inRandomOrder()->value('id'),
+            'driver_id' => Driver::inRandomOrder()->value('id'),
+            'departure_time' => $departure,
+            'arrival_time' => $arrival,
+            'base_price' => fake()->randomFloat(2, 50, 300),
+            'total_seats' => 40,
         ];
     }
 }
