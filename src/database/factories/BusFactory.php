901e682d10a6116c455ccaf5d6c36028008106e2
SaiKYouBoi <iliaselggames@gmail.com>
2026-02-05 12:09:17 +0100

    refactor: BusFactory

---
+++ src/database/factories/BusFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BusCompany>
+ */
+class BusFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Bus::class;
+
+    public function definition()
+    {
+        $statuses = ['active', 'maintenance', 'retired'];
+
+        return [
+            'fleet_code' => 'BUS-' . $this->faker->unique()->numberBetween(1, 999),
+            'plate_number' => strtoupper($this->faker->unique()->bothify('???-###-??')),
+            'model' => $this->faker->randomElement(['Volvo 9800', 'Mercedes Tourismo', 'Scania Touring']),
+            'status' => $this->faker->randomElement($statuses),
+        ];
+    }
+}
